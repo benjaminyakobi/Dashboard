c6c8c430c5b7f126a6c792b49c1939186fd2cbdc
Benjamin Yakobi <benjaminyakobi@gmail.com>
2021-09-01 11:04:18 +0300

    Creating basic static table with mock data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with me!
-        </a>
-      </header>
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Adress</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Benjamin</td>
+            <td>Ashdod</td>
+            <td>972-972-9722</td>
+            <td>Benjamin@benj.com</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
