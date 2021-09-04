ed3cff9aca10a7f51c140f18ec6d740bbe328252
Benjamin Yakobi <benjaminyakobi@gmail.com>
2021-09-04 22:31:25 +0300

    Adding Sign-up container on top of the main page (bootstrap) & modified App.css

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import { nanoid } from 'nanoid'
 import React, { useState, useEffect } from 'react'
 import { db } from "../initFirebase"
 import { ref, push, set, onValue } from "firebase/database"
-
+import Signup from "./Signup"
+import { Container } from "react-bootstrap"
 
 const App = () => {
   const listsRef = ref(db, 'Lists') //Getting a reference to 'Lists' in Firebase-RT-DB
@@ -19,11 +20,11 @@ const App = () => {
   useEffect(() => {
     onValue(listsRef, (snapshot) => {
       const jsonObject = snapshot.val() //Getting each child value under 'Lists' as a json object
-      try{
+      try {
         const listObject = Object.values(jsonObject) //Convert a json object to a list of jsons
         setContacts(listObject) //Updating state: 'contacts' using setContacts
       }
-      catch{
+      catch {
         console.log("Database is empty")
       }
     })
@@ -63,56 +64,64 @@ const App = () => {
   }
 
   return (
-    <div className="app-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact) =>
-            <tr key={contact.id}>
-              <td>{contact.fullName}</td>
-              <td>{contact.address}</td>
-              <td>{contact.phoneNumber}</td>
-              <td>{contact.email}</td>
-            </tr>)}
-        </tbody>
-      </table>
-      <h2>Add a Contact</h2>
-      <form>
-        <input
-          type="text"
-          name="fullName"
-          required="required"
-          placeholder="Enter a name..."
-          onChange={handleAddFormChange} />
-        <input
-          type="text"
-          name="address"
-          required="required"
-          placeholder="Enter an address..."
-          onChange={handleAddFormChange} />
-        <input
-          type="text"
-          name="phoneNumber"
-          required="required"
-          placeholder="Enter a phone number..."
-          onChange={handleAddFormChange} />
-        <input
-          type="email"
-          name="email"
-          required="required"
-          placeholder="Enter an email..."
-          onChange={handleAddFormChange} />
+    <>
+      <Container className='signup-container'>
+          <div className='w-100' style={{ maxWidth: '400px' }}>
+            <Signup />
+          </div>
+      </Container>
 
-        <button type="reset" onClick={handleAddFormSubmit}>Add</button>
-      </form>
-    </div>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) =>
+              <tr key={contact.id}>
+                <td>{contact.fullName}</td>
+                <td>{contact.address}</td>
+                <td>{contact.phoneNumber}</td>
+                <td>{contact.email}</td>
+              </tr>)}
+          </tbody>
+        </table>
+        <h2>Add a Contact</h2>
+        <form className='form-container'>
+          <input
+            type="text"
+            name="fullName"
+            required="required"
+            placeholder="Enter a name..."
+            onChange={handleAddFormChange} />
+          <input
+            type="text"
+            name="address"
+            required="required"
+            placeholder="Enter an address..."
+            onChange={handleAddFormChange} />
+          <input
+            type="text"
+            name="phoneNumber"
+            required="required"
+            placeholder="Enter a phone number..."
+            onChange={handleAddFormChange} />
+          <input
+            type="email"
+            name="email"
+            required="required"
+            placeholder="Enter an email..."
+            onChange={handleAddFormChange} />
+
+          <button type="reset" onClick={handleAddFormSubmit}>Add</button>
+        </form>
+      </div>
+    </>
   )
 }
 
