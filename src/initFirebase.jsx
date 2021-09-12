822a9169e18aecaffc5cbb3a232b0dab970d0975
Benjamin Yakobi <benjaminyakobi@gmail.com>
2021-09-12 17:42:44 +0300

    Adding local env variables

--- src/initFirebase.jsx
+++ src/initFirebase.jsx
@@ -3,14 +3,22 @@ import { getAuth } from "firebase/auth"
 import { getDatabase } from "firebase/database"
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBhiHbrO2qUvGDmGN3iX5u-SlpRK_EPlzU",
-    authDomain: "table-app-e64c3.firebaseapp.com",
-    databaseURL: "https://table-app-e64c3-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "table-app-e64c3",
-    storageBucket: "table-app-e64c3.appspot.com",
-    messagingSenderId: "102169979497",
-    appId: "1:102169979497:web:c8dc8170bd6ce50f4850f8",
-    measurementId: "G-6RC7ZQ1177"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    // apiKey: "AIzaSyBhiHbrO2qUvGDmGN3iX5u-SlpRK_EPlzU",
+    // authDomain: "table-app-e64c3.firebaseapp.com",
+    // databaseURL: "https://table-app-e64c3-default-rtdb.europe-west1.firebasedatabase.app",
+    // projectId: "table-app-e64c3",
+    // storageBucket: "table-app-e64c3.appspot.com",
+    // messagingSenderId: "102169979497",
+    // appId: "1:102169979497:web:c8dc8170bd6ce50f4850f8",
+    // measurementId: "G-6RC7ZQ1177"
   };
 
   const app = initializeApp(firebaseConfig)
