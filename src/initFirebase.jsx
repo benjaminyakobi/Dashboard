07f1c0b5bcf6316b4067cb0205ff76a61c1503b8
Benjamin Yakobi <benjaminyakobi@gmail.com>
2021-09-12 18:25:53 +0300

    BUILD UPDATE: Adding local env variables

--- src/initFirebase.jsx
+++ src/initFirebase.jsx
@@ -11,14 +11,6 @@ import { getDatabase } from "firebase/database"
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-    // apiKey: "AIzaSyBhiHbrO2qUvGDmGN3iX5u-SlpRK_EPlzU",
-    // authDomain: "table-app-e64c3.firebaseapp.com",
-    // databaseURL: "https://table-app-e64c3-default-rtdb.europe-west1.firebasedatabase.app",
-    // projectId: "table-app-e64c3",
-    // storageBucket: "table-app-e64c3.appspot.com",
-    // messagingSenderId: "102169979497",
-    // appId: "1:102169979497:web:c8dc8170bd6ce50f4850f8",
-    // measurementId: "G-6RC7ZQ1177"
   };
 
   const app = initializeApp(firebaseConfig)
